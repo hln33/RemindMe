d7c9890477f975d160376a978e484a5c87a0f94c
harryiscool123 <harrynguyen019@gmail.com>
2021-11-11 17:15:32 -0800

    implemented countdown timer for reminders

--- src/components/Reminder.js
+++ src/components/Reminder.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react' 
 
 function Reminder({reminder}) {
-    const [timeRemaining, setTimeRemaining] = useState('')
+    const [timeRemaining, setTimeRemaining] = useState({})
 
     const findTimeLeft = () => {
         const remindDate = reminder.date
         const present = new Date()
-        const secDiff = (remindDate.getTime() - present.getTime() ) / 1000
+        const secDiff = (remindDate.getTime() - present.getTime()) / 1000
         let timeLeft = {}
 
         if (secDiff > 0) {
@@ -16,16 +16,19 @@ function Reminder({reminder}) {
                 minutes: Math.floor((secDiff / 60) % 60),
                 seconds: Math.floor(secDiff % 60)
             }
-            console.log(timeLeft)
+            setTimeRemaining(timeLeft)
         }
     }
 
-    findTimeLeft()
+    useEffect(() => {
+        setInterval(() => findTimeLeft(), 1000)
+    }, [])
+
 
     return (
         <div>
             <h3>{reminder.text}</h3>
-            <h3>Date Added: {reminder.date.toLocaleString()}</h3>
+            <h3>Time until Reminded: {timeRemaining.days} D {timeRemaining.hours} H {timeRemaining.minutes} M {timeRemaining.seconds} S</h3>
         </div>
     )
 }
