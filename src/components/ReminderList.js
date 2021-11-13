5a3982a8f1bd125e27df168aaab91f97b241cf4e
harryiscool123 <harrynguyen019@gmail.com>
2021-11-12 20:34:44 -0800

    added conditonal rendering for list title

--- src/components/ReminderList.js
+++ src/components/ReminderList.js
@@ -5,7 +5,7 @@ function ReminderList({ reminderList }) {
 
     return (
         <div>
-            <h2>Pending Reminders</h2>
+            <h2>{reminderList.length > 0 ? 'Pending Reminders' : null}</h2>
             {reminderList.map( r => <Reminder key={r.id} reminder={r}/> )}
         </div>
     )
