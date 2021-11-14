52376482da95b88ad45e450cdc5009d992e579bf
harryiscool123 <harrynguyen019@gmail.com>
2021-11-13 20:37:24 -0800

    fixed typo in console.log()

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    
+
 })
 
-app.listen(3001, () => console.log('server started on port 3000'))
+app.listen(3001, () => console.log('server started on port 3001'))
