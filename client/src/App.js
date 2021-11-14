7d1491952cc167f512def0a13314b4843cc4ad67
harryiscool123 <harrynguyen019@gmail.com>
2021-11-13 20:57:04 -0800

    began connecting front and back end

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,29 @@ import './App.css';
 
 function App() {
   const [reminders, setReminders] = useState([])
-  useEffect(() => console.log(reminders), [reminders])
   useEffect(() => {
-    axios.get('http://localhost:3001/api').then((res) => {
-     
+    axios.get('http://localhost:3001/api').then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
     })
   }, [])
 
   const handleAddition = newReminder => {
-    axios.post('http://localhost:3001/api')
+    axios.post('http://localhost:3001/api', newReminder).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+
     setReminders([newReminder, ...reminders])
   }
   const handleRemoval = id => {
-    axios.delete(`http://localhost:3001/api/${id}`)
+    axios.delete(`http://localhost:3001/api/${id}`).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
     setReminders(reminders.filter(r => r.id !== id))
   }
 
--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
+    console.log("get working")
+})
 
+app.post('/api', (req, res) => {
+    console.log("post working")
 })
 
 app.listen(3001, () => console.log('server started on port 3001'))
