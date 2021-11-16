9afb416bdcb5e16c6ec16b8077f62d8390befb40
harryiscool123 <harrynguyen019@gmail.com>
2021-11-15 21:46:52 -0800

    made export explicit in httpCalls.js

--- client/src/httpCalls.js
+++ client/src/httpCalls.js
@@ -1,19 +1,21 @@
 import axios from 'axios'
 
-export const getReminders = () => {
+const getReminders = () => {
     axios.get('http://localhost:3001/api')
       .then(res => console.log(res))
       .catch(err => console.log(err))
 }
   
-export const postReminder = (newReminder) => {
+const postReminder = (newReminder) => {
     axios.post('http://localhost:3001/api', newReminder)
       .then(res => console.log(res))
       .catch(err => console.log(err))
 }
   
-export const deleteReminder = (id) => {
+const deleteReminder = (id) => {
     axios.delete(`http://localhost:3001/api/${id}`)
       .then(res => console.log(res))
       .catch(err => console.log(err))
 }
+
+export {getReminders, postReminder, deleteReminder}
